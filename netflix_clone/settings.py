2b97d1c09b4c5bad2e6e36897f2f504dc7d62dc6
Than Trung Tam <thantam311@gmail.com>
2025-04-09 08:38:42 +0700

    database mysql

--- netflix_clone/settings.py
+++ netflix_clone/settings.py
@@ -84,15 +84,15 @@ WSGI_APPLICATION = 'netflix_clone.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'mysql_db': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'film_database',
         'USER': 'root',
-        'PASSWORD': 'redteam@dl47',
+        'PASSWORD': 'thantam@311',
         'HOST': 'localhost',
         'PORT': '3306',
     }
