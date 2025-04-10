87140def1f4caff3b83c926f56df115a4144171f
Than Trung Tam <thantam311@gmail.com>
2025-04-10 18:58:23 +0700

    add forget password page

--- netflix_clone/urls.py
+++ netflix_clone/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +11,12 @@ urlpatterns = [
     path('user/', include('user_interactions.urls')),
     path('accounts/', include('accounts.urls')),
     
+
+    # Reset password URL
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
